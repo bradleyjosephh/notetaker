c623dac2da922bdf539fc14f518282b9315fc8d5
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-14 20:02:04 -0800

    html routing set

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+var app = require('express').Router()
+var path = require('path')
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+
+app.get('/notes', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = app
